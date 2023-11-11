d90377894978f2b8227a9b6d9b6965596ccdc069
PATRICK-ADA <donpat646@gmail.com>
2023-11-11 21:10:25 +0100

    Updates

--- ServiceShare2/StreamingServices.Core/Abstraction/IUtubeService.cs
+++ ServiceShare2/StreamingServices.Core/Abstraction/IUtubeService.cs
@@ -9,6 +9,8 @@ public interface IUtubeService
 Task<UtubeService> GetServiceById(string serviceId);
 Task<UtubeService> GetServiceByServicePlan(string serviceType);
 Task<List<UtubeService>> GetServicesByUserId(string UserId);
+
+Task<int>  GetNumberOfUnusedMonthlyServices(int maxNumberOfMonthlyUsers);
   }
 
 }
--- ServiceShare2/StreamingServices.Domain/Models/UtubeService.cs
+++ ServiceShare2/StreamingServices.Domain/Models/UtubeService.cs
@@ -1,4 +1,6 @@
 
+using System.Dynamic;
+
 namespace SModel.UtubeService
 {
 public class UtubeService
@@ -9,6 +11,11 @@ public int WeeklyService {get; set;}  = 7;
 public int MonthlyService {get;set;} = 30;
 
 public int YearlyService  {get; set;} = 360;
+
+public int UtubeYearlyUsersCappacity {get; set;}
+public int UtubeMonthlyUsersCappacity {get; set;}
+
+public int UtubeWeeklyUsersCappacity {get; set;}
 
 public DateTime SubscriptionTime {get; set;} = DateTime.Now;
 
