bcd76f955c2a431f8ae4d71083b96e98ba8dad76
PATRICK-ADA <donpat646@gmail.com>
2023-11-15 15:01:56 +0100

    Updates

--- ServiceShare2/StreamingServices.Domain/Models/NetFlixService.cs
+++ ServiceShare2/StreamingServices.Domain/Models/NetFlixService.cs
@@ -5,18 +5,14 @@ namespace SModel.NetFlixService
 
 public class NetFlixService
 {
+
 public string NetFlixServiceId {get; set;} = Guid.NewGuid().ToString();
 
-public int WeeklyService {get; set;}  = 7;
-public int MonthlyService {get;set;} = 30;
-
 public int YearlyService  {get; set;} = 360;
+public int MonthlyService {get;set;} = 30;
+public int WeeklyService {get; set;}  = 7;
 
-public int NetFlixYearlyUsersCappacity {get; set;}
-
-public int NetFlixMonthlyUsersCappacity {get; set;}
-
-public int NetFlixWeeklyUsersCappacity {get; set;}
+public int NumberOfUsersRegisteredForAServicePlan{get; set;}
 
 public DateTime SubscriptionTime {get; set;} = DateTime.Now;
 
